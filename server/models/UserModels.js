8d3bc4631889f991f6e4e910a2f532c12666c7e5
ISWARYA835 <iswarya83p@gmail.com>
2025-05-28 11:11:44 +0530

    changes

--- server/models/UserModels.js
+++ server/models/UserModels.js
@@ -1,3 +1,40 @@
 import mongoose from "mongoose";
+import {genSalt} from "bcrypt"
 
-const userSchema = new mongoose
+
+
+const userSchema = new mongoose.Schema({
+    email :{
+        type:String,
+        required:[true, "Email is Required."],
+        unique : true,
+    },
+    password : {
+        type :String,
+        required : [true , "Password is Rquired."],
+
+    },
+    lastname : {
+        type:String,
+        required:false,
+    },
+    image : {
+        type:String,
+        required:false,
+    },
+    color : {
+        type:Number,
+        required:false,
+    },
+    profileSetup : {
+        type:Boolean,
+        default:false,
+    },
+
+})
+userSchema.pre("save",async function(next){
+    const salt = await genSalt();
+    this.password = await hash(this.password,salt);
+    next();
+
+})
